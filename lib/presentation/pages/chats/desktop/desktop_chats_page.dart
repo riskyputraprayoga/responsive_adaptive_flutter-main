02824ed7f65c968f21a234f18791ec0251e3dac9
Yudiantara11 <antarayudi568@gmail.com>
2023-06-14 09:05:18 +0800

    File Awal Aplikasi

--- lib/presentation/pages/chats/desktop/desktop_chats_page.dart
+++ lib/presentation/pages/chats/desktop/desktop_chats_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_flutter/presentation/pages/chats/desktop/widgets/chat_page_widget.dart';
 import 'package:responsive_flutter/presentation/pages/chats/desktop/widgets/side_bar.dart';
+import 'package:responsive_flutter/presentation/pages/chats/mobile/widgets/bottom_navigation_bar_widget.dart';
 
 class DesktopChatsPage extends StatelessWidget {
   const DesktopChatsPage({super.key});
@@ -8,6 +9,7 @@ class DesktopChatsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: const BottomNavigationBarWidget(),
       body: Row(
         children: const [
           SideBar(),
